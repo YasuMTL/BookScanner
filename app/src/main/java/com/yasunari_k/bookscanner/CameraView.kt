1c5466ac37826266f3cb591ac8b139ce62fff133
Yasunari <45267210+YasuMTL@users.noreply.github.com>
2023-03-07 10:17:31 -0500

    Try to close barcodeScanner when you press on back button to get out of the camera view screen.

--- app/src/main/java/com/yasunari_k/bookscanner/CameraView.kt
+++ app/src/main/java/com/yasunari_k/bookscanner/CameraView.kt
@@ -10,6 +10,7 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +18,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
@@ -71,6 +74,29 @@ fun CameraView(
         }
     )
 
+    // If `lifecycleOwner` changes, dispose and reset the effect
+    DisposableEffect(lifecycleOwner) {
+        // Create an observer that triggers our remembered callbacks
+        // for sending analytics events
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                consoleLog("on start")
+            } else if (event == Lifecycle.Event.ON_STOP) {
+                consoleLog("on stop")
+            }
+        }
+
+        // Add the observer to the lifecycle
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // When the effect leaves the Composition, remove the observer
+        onDispose {
+            barcodeScanner.close()//test
+            consoleLog("The effect is leaving the Composition")
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
     cameraController.bindToLifecycle(lifecycleOwner)
     previewView.controller = cameraController
 
@@ -85,4 +111,8 @@ fun CameraView(
 private fun beep() {
     val toneGen = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
     toneGen.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200)
+}
+
+private fun consoleLog(messageToLog: String) {
+    Log.i("CameraView.kt", messageToLog)
 }
