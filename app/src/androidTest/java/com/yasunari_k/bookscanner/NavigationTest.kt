5809bf32386a4ff240a365212d94bc9ac5817a4c
Yasunari <45267210+YasuMTL@users.noreply.github.com>
2023-03-07 11:14:29 -0500

    Add first navigation test

---
+++ app/src/androidTest/java/com/yasunari_k/bookscanner/NavigationTest.kt
@@ -0,0 +1,37 @@
+package com.yasunari_k.bookscanner
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupBookScannerNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            BookScannerNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun bookScannerNavHost_verifyMainDisplayedAsStartDestination() {
+        composeTestRule
+            .onNodeWithContentDescription("Main Screen")
+            .assertIsDisplayed()
+    }
+}
