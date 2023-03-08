1086342f3b9feaf169adf6af8f3715f52e9f05e9
Yasunari <45267210+YasuMTL@users.noreply.github.com>
2023-03-08 08:56:40 -0500

    Add contentDescription to the following screens: - BorrowScreen.kt - LoggedInScreen.kt - ReturnScreen.kt

--- app/src/main/java/com/yasunari_k/bookscanner/ui/returns/ReturnScreen.kt
+++ app/src/main/java/com/yasunari_k/bookscanner/ui/returns/ReturnScreen.kt
@@ -9,13 +9,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 
 @Composable
 fun ReturnScreen() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(),
+            .fillMaxHeight()
+            .semantics { contentDescription = "Return Screen"},
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
