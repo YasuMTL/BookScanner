0d558eefa63669c2ba54727afed35c852fd6da07
Yasunari <45267210+YasuMTL@users.noreply.github.com>
2023-03-09 10:13:03 -0500

    Add Moshi

--- app/src/main/java/com/yasunari_k/bookscanner/network/BookApiService.kt
+++ app/src/main/java/com/yasunari_k/bookscanner/network/BookApiService.kt
@@ -1,21 +1,21 @@
 package com.yasunari_k.bookscanner.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val BASE_URL =
     "https://www.googleapis.com/books/v1/volumes/"
-//private val moshi = Moshi.Builder()
-//    .add(KotlinJsonAdapterFactory())
-//    .build()
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private val retrofit = Retrofit.Builder()
-//    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    //.addConverterFactory(GsonConverterFactory.create())
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
