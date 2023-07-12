b99d84d1e29bfc0c48c89b6100ad1696adeeee11
Yasunari <yasunari.k@hotmail.com>
2023-07-12 14:53:34 -0400

    correct the graphic interface of the list

--- app/src/main/java/com/yasunari_k/bookscanner/ui/returns/ReturnScreen.kt
+++ app/src/main/java/com/yasunari_k/bookscanner/ui/returns/ReturnScreen.kt
@@ -1,5 +1,6 @@
 package com.yasunari_k.bookscanner.ui.returns
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,20 +8,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yasunari_k.bookscanner.model.BookBorrower
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun ReturnScreen(
@@ -30,6 +39,7 @@ fun ReturnScreen(
 ) {
     val borrowedBooks = borrowerState.value.borrowedBooksList
     //TODO Fetch the borrowed books list first
+    Log.d("ReturnScreen", "borrowedBooks=$borrowedBooks")
 
     Column(
         modifier = Modifier
@@ -54,7 +64,9 @@ fun ReturnScreen(
             onClick = onClickBackButton,
             modifier = Modifier
                 .width(200.dp)
-                .height(60.dp)) {
+                .height(50.dp)
+                .padding(5.dp)
+        ) {
             Text(text = "Back")
         }
     }
@@ -67,12 +79,30 @@ fun BorrowedBookListItem(
 ) {
     val context = LocalContext.current
 
-    Row {
-        Column {
-            Text(text = borrowedBook.title)
+    Row (verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ){
+        Column (
+            modifier = Modifier.
+                weight(3F),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = borrowedBook.title,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Medium
+            )
             Text(text = borrowedBook.dateToReturn)
         }
         Button(
+            modifier = Modifier.
+                //size(height = 50.dp).
+                height(50.dp).
+                padding(5.dp).
+                weight(2F),
             onClick = {
                 Toast.makeText(context, "${borrowedBook.title}を本当に返却してもよろしいですか？", Toast.LENGTH_SHORT).show()
                 //TODO
@@ -93,15 +123,33 @@ fun BorrowedBookListItem(
     }
 }
 
-//@Preview
-//@Composable
-//fun PreviewReturnScreen() {
-//    MaterialTheme {
-//        ReturnScreen(
-//            {/*DO nothing*/},
-//            DataProvider.borrowedBookList)
-//    }
-//}
+@Preview
+@Composable
+fun PreviewReturnScreen() {
+    MaterialTheme {
+        ReturnScreen(
+            onClickBorrowedBook = { bookTitle ->
+
+            },
+            onClickBackButton = {
+
+            },
+            borrowerState = MutableStateFlow(
+                BookBorrower(
+                    name = "Test Yasu",
+                    emailAddress = "aaabbb@com",
+                    registerDate = "2023-07-12",//todo: No more need the "registerDate"
+                    borrowedBooksList = mutableListOf(
+                        BorrowedBook("Test Yasu", "Test 1", "Someday"),
+                        //BorrowedBook("Kanemitsu", "Mini dictionnaire Français-Allemand Allemand-Français", "2023-07-08 16:59"),
+                        BorrowedBook("Kanemitsu", "Mini dictionnaire Français-Allemand aaaa", "2023-07-08 16:59"),
+                        BorrowedBook("Test Yasu", "Test 3", "Someday")
+                    )
+                )
+            ).asStateFlow().collectAsState()
+        )
+    }
+}
 
 //Some data to mock the presentation
 data class BorrowedBook(
